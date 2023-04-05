6555e4bdf3d1735a9b5c5455f9e9e76cb10bbe81
Scotthurwitz1 <hurwitzscott@gmail.com>
2023-04-04 22:33:37 -0400

    start

---
+++ src/software1/Software1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package software1;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author scottsmac
+ */
+public class Software1 extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    }
+    
+}
